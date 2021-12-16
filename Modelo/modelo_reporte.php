1fd281fd2e3c2d7a69eca5e531f3066795f26cfe
Tony González <73860264+TonyG24@users.noreply.github.com>
2021-12-16 18:10:11 -0500

    Repolte

--- Modelo/modelo_reporte.php
+++ Modelo/modelo_reporte.php
@@ -14,25 +14,19 @@ session_start();
             require('../../vista/plugins/fpdf/fpdf.php');
             $pdf = new FPDF();
             $pdf->AddPage();
-            $pdf->Image('../../vista/imagenes/amarillo.jpg' , 0 ,0, 0 , 25,'jpg');
             
             $pdf->SetFont('Arial','B',12);
-            $pdf->Image('../../vista/imagenes/logo-alcaldia.png' , 4 ,1, 0 , 3,'png');
+            $pdf->Image('../../vista/imagenes/logo-alcaldia.png' , 18 ,5, 0 , 30,'png');
             $pdf->SetFont('Arial','B',15);
-            $pdf->Cell(27.5,1,'ALCALDIA DE IBAGUE',0,1,'C');
+            $pdf->Cell(200,10,'ALCALDIA DE IBAGUE',0,1,'C');
             $pdf->SetFont('Arial','B',10);
-            $pdf->Cell(27.5,0,'SECRETARIA DE TRANSITO Y TRANSPORTE DE LA MOVILIDAD',0,1,'C');
+            $pdf->Cell(200,5,'SECRETARIA DE TRANSITO Y TRANSPORTE DE LA MOVILIDAD',0,1,'C');
             $pdf->SetFont('Arial','B',10);
             $pdf->SetTextColor(2,8,4);
-            $pdf->Cell(27.5,1,'Tarjeta de Control Numero',0,1,'C');
-            $pdf->Image('../../vista/imagenes/musical.png' , 23 ,1, 0 , 3,'png');
+            $pdf->Cell(200,5,'Tarjeta de Control Numero',0,1,'C');
+            $pdf->Image('../../vista/imagenes/musical.png' , 170 ,5, 0 , 30,'png');
             
-            $pdf->Ln(2);
-            $pdf->Cell(3,3,'',0,0,'L',false);
-            $pdf->Cell(3.2,3.5,'',1,0,'L',false);
-            $pdf->Cell(1,1,'',0,0,'L',false);
-            $pdf->SetFont('Arial','B',9);
-            $pdf->SetTextColor(2,2,3);
+
 
             
 
